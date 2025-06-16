242057dee96fd18908142e33d16ac5411e592630
Davide <davide.albers@hotmail.de>
2025-06-16 13:28:31 +0200

    testing branches

--- R/goodbye.R
+++ R/goodbye.R
@@ -1,3 +1,14 @@
-goodbye = function () {
+#' Split a string
+#'
+#' @param x A character vector with one element.
+#' @param split What to split on.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x, split = ",")
+goodbye <- function() {
   print("goodbye")
 }
